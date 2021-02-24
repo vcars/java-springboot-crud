6438faec8c6444495fef56041f9f01a5565f5ff1
SUPPORTING\C090221001 <ahmad.zulfikar@bankmandiri.co.id>
2021-02-24 21:16:21 +0700

    init

---
+++ src/main/java/com/bootcamp/javaspringbootcrud/JavaSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.javaspringbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/javaspringbootcrud/JavaSpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.javaspringbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
